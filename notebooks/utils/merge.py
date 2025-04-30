d9f404ea7a8bbe2751f5ae2c86487e83a4195a04
Sea <xigart1@gmail.com>
2025-04-30 23:34:27 +0400

    chore(scripts): add another holidays source

--- notebooks/utils/merge.py
+++ notebooks/utils/merge.py
@@ -116,3 +116,37 @@ def merge_all_dfs(dfs: dict[str, pd.DataFrame]) -> pd.DataFrame:
     #     # estonia_holidays = holidays.EE(years=range(2021, 2024), language='en_US')
     #     # for date, name in estonia_holidays.items():
     #     #     print(date, name)
+
+
+# https://www.timeanddate.com/holidays/estonia/2021
+# Holidays and Observances in Estonia in 2021
+# Date	 	Name	Type
+# 1 Jan	Friday	New Year's Day	National Holiday
+# 6 Jan	Wednesday	Epiphany	Observance
+# 2 Feb	Tuesday	Anniversary of Tartu Peace Treaty	Observance
+# 24 Feb	Wednesday	Independence Day	National Holiday
+# 14 Mar	Sunday	Mother Tongue Day	Observance
+# 20 Mar	Saturday	March Equinox	Season
+# 2 Apr	Friday	Good Friday	National Holiday
+# 4 Apr	Sunday	Easter Sunday	National Holiday
+# 1 May	Saturday	Labor Day	National Holiday
+# 9 May	Sunday	Mothers' Day	Observance
+# 23 May	Sunday	Pentecost	National Holiday
+# 4 Jun	Friday	Flag Day	Observance
+# 14 Jun	Monday	Day of Mourning	Observance
+# 21 Jun	Monday	June Solstice	Season
+# 23 Jun	Wednesday	Victory Day	National Holiday
+# 24 Jun	Thursday	Midsummer Day	National Holiday
+# 20 Aug	Friday	Independence Restoration Day	National Holiday
+# 23 Aug	Monday	Day of Remembrance for Victims of Communism and Nazism	Observance
+# 12 Sep	Sunday	Grandparents' Day	Observance
+# 22 Sep	Wednesday	Resistance Day	Observance
+# 22 Sep	Wednesday	September Equinox	Season
+# 16 Oct	Saturday	Finno-Ugric Day	Observance
+# 2 Nov	Tuesday	All Soul's Day	Observance
+# 14 Nov	Sunday	Father's Day	Observance
+# 16 Nov	Tuesday	Day of Declaration of Sovereignty	Observance
+# 21 Dec	Tuesday	December Solstice	Season
+# 24 Dec	Friday	Christmas Eve	National Holiday
+# 25 Dec	Saturday	Christmas Day	National Holiday
+# 26 Dec	Sunday	Boxing Day	National Holiday
