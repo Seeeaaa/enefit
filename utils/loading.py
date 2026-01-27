03ac9cd79eed9091296577d492404a37a1cea2ec
Sea <xigart1@gmail.com>
2026-01-27 22:24:12 +0400

    feat(loading): add load_parquet_data

--- utils/loading.py
+++ utils/loading.py
@@ -2,9 +2,9 @@ import pandas as pd
 from pandas import DataFrame, Series
 
 
-def load_all_raw_data(
+def load_raw_data(
     main_path: str, additional_path: str | None = None
-) -> dict[str, DataFrame | Series]:
+) -> dict[str, DataFrame]:
     """
     Load all raw datasets from competition and optional external
     sources.
@@ -20,7 +20,7 @@ def load_all_raw_data(
 
     Returns
     -------
-    dict[str, DataFrame | Series]
+    dict[str, DataFrame]
         A dictionary where keys are dataset names (e.g., 'train',
         'client', 'holidays'), and values are either DataFrame or
         Series.
@@ -36,14 +36,44 @@ def load_all_raw_data(
         "historical_weather": pd.read_csv(
             f"{main_path}historical_weather.csv"
         ),
-        "station_county_mapping": pd.read_csv(
+        "weather_station_to_county_mapping": pd.read_csv(
             f"{main_path}weather_station_to_county_mapping.csv"
         ),
-        "county_id_to_name_map": pd.read_json(
-            f"{main_path}county_id_to_name_map.json", typ="series"
-        ),
+        # "county_id_to_name_map": pd.read_json(
+        #     f"{main_path}county_id_to_name_map.json", typ="series"
+        # ),
     }
     if additional_path:
         data["holidays"] = pd.read_csv(f"{additional_path}holidays.csv")
+
+    return data
+
+
+def load_parquet_data(parquet_path: str) -> dict[str, DataFrame]:
+    """
+    Load raw data in parquet format.
+
+    Returns
+    -------
+    dict[str, DataFrame]
+        A dictionary where keys are dataset names (e.g., 'train'), and
+        values are DataFrames.
+    """
+    data = {
+        "train": pd.read_parquet(f"{parquet_path}train"),
+        "gas_prices": pd.read_parquet(f"{parquet_path}gas_prices"),
+        "client": pd.read_parquet(f"{parquet_path}client"),
+        "electricity_prices": pd.read_parquet(
+            f"{parquet_path}electricity_prices"
+        ),
+        "forecast_weather": pd.read_parquet(f"{parquet_path}forecast_weather"),
+        "historical_weather": pd.read_parquet(
+            f"{parquet_path}historical_weather"
+        ),
+        "weather_station_to_county_mapping": pd.read_parquet(
+            f"{parquet_path}weather_station_to_county_mapping"
+        ),
+        "holidays": pd.read_parquet(f"{parquet_path}holidays"),
+    }
 
     return data
